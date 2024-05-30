0828676caf0b3ce3a8bd96b23e92ad1671ff4f49
P-Heckmann <paul.he@posteo.de>
2024-05-30 15:55:07 +0200

    setup

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Middle Earth",
+)
+
+st.write("# Welcome to Middle Earth")
+
+# st.sidebar.success("Select a feature.")
+
+st.markdown(
+    """
+    ---
+    ### Info
+
+    ---
+    By [Paul Heckmann](https://www.paulheckmann.de/)
+"""
+)
